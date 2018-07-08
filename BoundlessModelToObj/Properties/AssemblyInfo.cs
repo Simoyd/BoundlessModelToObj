5b8ae74bce05f4311841a636ec22efc3d7ed412c
cwilliams <simoyd@rogers.com>
2018-07-08 09:18:47 -0400

    some cleanup

--- BoundlessModelToObj/Properties/AssemblyInfo.cs
+++ BoundlessModelToObj/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BoundlessModelToObj")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyCopyright("Copyright © Carter Williams 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
